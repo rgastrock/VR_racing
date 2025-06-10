a16c7a389e32b381997f558730866ad91406f6e8
rgastrock <raphaelgastrock@gmail.com>
2025-06-10 16:29:12 -0400

    update OSF with new data

--- ana/downloadData.R
+++ ana/downloadData.R
@@ -3,7 +3,7 @@ source('ana/shared.R')
 getBehaviorData <- function(){
   
   Reach::downloadOSFdata(repository = 'w5d4u',
-                         filelist = list('data/' =c('T-RACING_0.zip', 'T-RACING_180.zip', 'T-RACING_90.zip', 'T-RACING_270.zip')),
+                         filelist = list('data/' =c('T-RACING_0.zip', 'T-RACING_180.zip', 'T-RACING_90.zip', 'T-RACING_270.zip', 'data_run2.zip')),
                          folder = 'data/',
                          overwrite = TRUE,
                          unzip = TRUE,
